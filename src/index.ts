e57a44fa249542d946b075b0b3049596f09cfbcc
Sagar Davara <sagardspeed2@gmail.com>
2025-04-15 19:14:24 +0530

    add specific format order id in get orders

--- src/index.ts
+++ src/index.ts
@@ -51,6 +51,7 @@ function formatProduct(product: ProductNode): string {
 function formatOrder(order: ShopifyOrderGraphql): string {
   return `
   Order: ${order.name} (${order.id})
+  Order Id: ${order.id.split("/").pop()}
   Created At: ${order.createdAt}
   Status: ${order.displayFinancialStatus || "N/A"}
   Email: ${order.email || "N/A"}
